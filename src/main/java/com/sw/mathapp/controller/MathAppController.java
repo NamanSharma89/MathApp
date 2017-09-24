5ecb47f69ef0821fca6848a9ee0bc632030efa6e
NamanSharma89 <dev.namansharma89@gmail.com>
2017-09-24 23:34:17 +0530

    yet aonther commit

--- src/main/java/com/sw/mathapp/controller/MathAppController.java
+++ src/main/java/com/sw/mathapp/controller/MathAppController.java
@@ -1,14 +1,20 @@
 package com.sw.mathapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.sw.mathapp.service.SumOfAllDivisors;
+
 @Controller
 public class MathAppController {
-// TODO: Look into auto-wiring
-	@RequestMapping(value="/sumofdivisors")
-	public void getSumOfDivisors(){
-		
+	// TODO: Look into auto-wiring
+	@Autowired
+	SumOfAllDivisors sumOfAllDivisors;
+
+	@RequestMapping(value = "/sumofdivisors")
+	public void getSumOfDivisors() {
+
 	}
-	
+
 }
