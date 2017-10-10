ad86e2efe8a22f80c80e6f8b78ae6434e6160709
NamanSharma89 <dev.namansharma89@gmail.com>
2017-10-10 22:35:30 +0530

    playing around

--- src/main/java/com/sw/mathapp/configuration/MathAppConfiguration.java
+++ src/main/java/com/sw/mathapp/configuration/MathAppConfiguration.java
@@ -1,13 +1,16 @@
 package com.sw.mathapp.configuration;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
-//@Configuration
-//@EnableWebMvc
-//@ComponentScan(basePackages = "com.sw.mathapp")
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.sw.mathapp")
 public class MathAppConfiguration {
 
 	public void configureViewResolvers(final ViewResolverRegistry registry) {
--- src/main/java/com/sw/mathapp/controller/MathAppController.java
+++ src/main/java/com/sw/mathapp/controller/MathAppController.java
@@ -9,9 +9,14 @@ import com.sw.mathapp.service.SumOfAllDivisors;
 
 @RestController
 public class MathAppController {
-	// TODO: Look into auto-wiring
+	// TODO: Look into auto-wiring and how it works with DI?
 	@Autowired
 	SumOfAllDivisors sumOfAllDivisors;
+
+	/*
+	 * public MathAppController(final SumOfAllDivisors sumOfAllDivisors) { // TODO:
+	 * Look into super again. super(); this.sumOfAllDivisors = sumOfAllDivisors; }
+	 */
 
 	@GetMapping("/sumofdivisors/{numberForDivisorSum}")
 	public int getSumOfDivisorsController(@PathVariable(value = "numberForDivisorSum") final int numberForDivisorSum) {
