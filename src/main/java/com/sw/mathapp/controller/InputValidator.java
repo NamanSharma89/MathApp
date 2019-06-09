d96e3051642fdcde5010ff1b6e887e892e2ad5b9
nash7 <dev.namansharma@gmail.com>
2019-06-09 23:04:42 +0530

    adding validator class component

---
+++ src/main/java/com/sw/mathapp/controller/InputValidator.java
@@ -0,0 +1,18 @@
+package com.sw.mathapp.controller;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class InputValidator {
+
+	public boolean validateInput(final String inputString) {
+	//	String checkInputForCommaIntValue = "[0-9,]+";
+	//	String checkInputForDoubleValues = "^[^.]+$";
+	//	boolean resultOfInputStringValidation = sortNumberInput.matches(checkInputForCommaIntValue);
+	//	boolean resultOfcheckInputForDoubleValues = sortNumberInput.matches(checkInputForDoubleValues);
+
+		
+		return true;
+	}
+
+}
