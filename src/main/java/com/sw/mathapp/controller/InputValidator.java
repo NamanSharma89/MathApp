9d5bff331aedc3653adaac8919e6f2fe9e5655ed
nash7 <dev.namansharma@gmail.com>
2019-09-15 21:27:40 +0530

    fixing regex for input validator

--- src/main/java/com/sw/mathapp/controller/InputValidator.java
+++ src/main/java/com/sw/mathapp/controller/InputValidator.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class InputValidator {
 
-	private static String COMMA_INT_CHECK = "^(?!.*,(.).*\\1)[0-9,]+$";
+	private static String COMMA_INT_CHECK = "^[0-9,]+$";
 
 	public boolean validate(String toValidate) {
 		boolean resultOfInputStringValidation = toValidate.matches(COMMA_INT_CHECK);
